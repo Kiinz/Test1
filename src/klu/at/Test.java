a3424986e57b0c58ef7a224b80c3c74b42051fe4
Kiinz <Alexander@Alexander-PC.lan>
2012-12-08 13:41:33 +0100

    Hallo

--- src/klu/at/Test.java
+++ src/klu/at/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Mein Name lautet Alexander Kainz");
+		System.out.println("Mein Name lautet");
 
 	}
 
